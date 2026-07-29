6435003b11d2837872d615d72bced270078740cf
Oklilin <rafaelborges.pelotas@gmail.com>
2026-07-29 17:46:29 -0300

    Implementada listagem de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -60,8 +60,43 @@ public class ProdutosDAO {
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
-        return listagem;
+         ArrayList<ProdutosDTO> lista = new ArrayList<>();
+
+    String sql = "SELECT * FROM produtos";
+
+    conn = new conectaDAO().connectDB();
+
+    try {
+
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            lista.add(produto);
+        }
+
+        resultset.close();
+        prep.close();
+        conn.close();
+
+    } catch (Exception e) {
+
+        JOptionPane.showMessageDialog(null,
+                "Erro ao listar produtos: " + e.getMessage());
+
     }
+
+    return lista;
+}
+     
     
     
     
